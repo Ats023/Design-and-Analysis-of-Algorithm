f24f795a661b57f63590d9e534d83a208e79d98a
Atryee Dhar <122550503+Ats023@users.noreply.github.com>
2023-02-27 22:31:14 +0530

    Update quick-sort.c

--- quick-sort.c
+++ quick-sort.c
@@ -1,35 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
-void sort(int*,int,int);
-void accept(int*,int);
-void disp(int*,int);
+
 void swap(int*x,int i,int j)
 {
 	int t=x[i];
 	x[i]=x[j];
 	x[j]=t;
 }
-int main()
-{
-	int *a,n;
-	printf("\nEnter number of elements: ");
-	scanf("%d",&n);
-	a=(int*)malloc(sizeof(int)*n);
-	accept(a,n);
-	sort(a,0,n-1);
-	printf("\nThe sorted array is\n");
-	disp(a,n);
-}
-void accept(int *x,int y)
-{
-	int i;
-	printf("Enter elements of Array:\n");
-	for(i=0;i<y;i++)
-	{
-		scanf("%d",&x[i]);
-	}
-}
-void sort(int*a,int left,int right)
+
+void quicksort(int*a,int left,int right)
 {
 	int i,last;
 	if(left>=right)
@@ -40,13 +19,22 @@ void sort(int*a,int left,int right)
 		if(a[left]>a[i])
 			swap(a,++last,i);
 	swap(a,last,left);
-	sort(a,left,last-1);
-	sort(a,last+1,right);	
+	quicksort(a,left,last-1);
+	quicksort(a,last+1,right);	
 }
-void disp(int *a,int n)
+
+void main()
 {
-	int i;
+	int *a,n;
+	printf("\nEnter number of elements: ");
+	scanf("%d",&n);
+	a=(int*)malloc(sizeof(int)*n);
+	printf("Enter elements of Array:\n");
+	for(i=0;i<y;i++)
+		scanf("%d",&x[i]);
+	quicksort(a,0,n-1);
+	printf("\nThe sorted array is\n");
 	for(i=0;i<n;i++)
-	printf("%d ",a[i]);
-	printf("\n");
+		printf("%d ",a[i]);
+	printf("\n");	
 }
